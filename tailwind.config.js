a79d0ae51c226af1b34a5637975bb4fa2d8abcd0
Rakibul Hasan <rifat20089@hotmail.com>
2021-09-04 16:45:49 +0600

    hero section done

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        mRed: {
+          light: '#f5afb4',
+          DEFAULT: '#E63643',
+          dark: '#5c161b',
+          hover: '#cf313c',
+        }
+      },
+    },
+    fontFamily: {
+      Poppins: ['Poppins', 'sans-serif'],
+      Bebas: ['Bebas Neue', 'sans-serif']
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+      screens: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px"
+      }
+    },
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['active'],
+      translate: ['active'],
+    },
+  },
+  plugins: [],
+}
